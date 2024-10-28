e84c4000db341bf127bdc1cc17d2218befc4992a
hadbet <hadbet.altamirano@grammer.com>
2024-10-28 11:41:17 -0600

    Carga de librerias y daos para las funciones

---
+++ dao/db/db_Inventario.php
@@ -0,0 +1,18 @@
+<?php
+
+class LocalConector
+{
+    private $host = "127.0.0.1:3306";
+    private $usuario = "u909553968_UserInventario";
+    private $clave = "Grammer2024Inventario";
+    private $db = "u909553968_Inventario";
+    public $conexion;
+
+    public function conectar()
+    {
+        $con = mysqli_connect($this->host, $this->usuario, $this->clave, $this->db);
+        return $con;
+    }
+}
+
+?>
